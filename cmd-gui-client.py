1423ccbfdb4c26ce672f227b3068282d0261c57b
Andrey Bazhenov <quarussique@yandex.ru>
2022-06-16 18:01:31 +0500

    :feat: add prepairing for a game

---
+++ cmd-gui-client.py
@@ -0,0 +1,73 @@
+import requests
+import sys
+from pprint import pformat, pprint
+import pyautogui
+import time
+
+
+if __name__ == '__main__':
+    TEST_MODE = True
+    session = requests.Session()
+    if TEST_MODE:
+        username = 'TestUserName'
+        server_url = 'http://127.0.0.1:5001'
+    else:
+        username = pyautogui.prompt('Введите ник')
+        server_url = pyautogui.prompt('Введите URL сервера')
+
+    client_id = session.post(server_url + '/register', params={'player_name': username}).json()['result']
+
+    game_chosen = False
+    while not game_chosen:
+        # получить список игр
+        response = session.get(server_url + '/get_games').json()
+        if response['status'] == 'ok':
+            games = response['result']
+        else:
+            pyautogui.alert('Ошибка получения списка игр. {}'.format(pformat(response)))
+            sys.exit()
+        action = ''
+        if len(games) > 0:
+            buttons_with_games_ids = []
+            for i in games:
+                buttons_with_games_ids.append(i['id']+' '+i['players'])
+            buttons_with_games_ids.append('+')
+            action = pyautogui.confirm(
+                text='Найдено игр: {}'.format(len(games)),
+                buttons=buttons_with_games_ids
+            )
+            if action == '+':
+                # создать новую, несмотря на существующие
+                amount_of_players = pyautogui.confirm(
+                    text='Сколько игроков?',
+                    buttons=[2, 3, 4]
+                )
+                response = session.post(server_url + '/create_game',
+                                        params={'amount_of_players': amount_of_players}).json()
+            else:
+                # получен id желаемой игры
+                game_chosen = action.split()[0]
+        else:
+            # игр нет, создать новую
+            amount_of_players = pyautogui.confirm(
+                text='Не найдено игр, создаю новую. На сколько игроков?',
+                buttons=[2, 3, 4]
+            )
+            response = session.post(server_url + '/create_game', params={'amount_of_players': amount_of_players})
+    response = session.post(server_url + '/enlist_for_game', params={'game_id': game_chosen, 'player_id': client_id})
+    pyautogui.alert('Вы зашли в игру. Нажмите ОК для ожидания старта.')
+    game_started = False
+    while not game_started:
+        time.sleep(1)
+        response = session.get(server_url + '/game_state', params={'game_id': game_chosen}).json()
+        if response['result']['is_started']:
+            game_started = True
+    print('Игра началась')
+    game_cycle = True
+    while game_cycle:
+        time.sleep(1)
+        response = session.get(server_url + '/game_state', params={'game_id': game_chosen}).json()
+        if response['result']['whose_move'] == client_id:
+            print('Ваш ход')
+        else:
+            pass
---
+++ server.py
@@ -0,0 +1,232 @@
+from fastapi import FastAPI
+import random
+import uvicorn
+
+
+CARD_NAMES = [
+    'Штурвал',
+    'Нож',
+    'Пистолет',
+    'Компас',
+    'Свеча',
+    'Попугай',
+    'Сундук',
+    'Ключ',
+    'Ром',
+    'Якорь',
+]
+
+
+class Game:
+    def __init__(self, max_players):
+        self.unique_id = str(random.randint(100000, 999999))
+        self.is_started = False
+        self.max_players = max_players
+        self.players_list = []
+        self.whose_move = None
+        self.card_decks = {
+            'treasure': [],
+            'raid': [],
+            'drop': []
+        }
+        for name in CARD_NAMES:
+            for i in range(6):
+                if i == 0:
+                    deck = 'drop'
+                else:
+                    deck = 'treasure'
+                if name == 'Попугай':
+                    self.card_decks[deck].append(name + ' ' + str(i+4))
+                else:
+                    self.card_decks[deck].append(name + ' ' + str(i+2))
+
+    def enlist_player(self, player_id):
+        if len(self.players_list) < self.max_players:
+            self.players_list.append(player_id)
+            if len(self.players_list) == self.max_players:
+                self.get_started()
+            return True
+        else:
+            self.get_started()
+            return False
+
+    def get_started(self):
+        self.is_started = True
+        for player_id in self.players_list:
+            self.card_decks[player_id] = {
+                'helms': [],
+                'knives': [],
+                'pistols': [],
+                'compasses': [],
+                'candles': [],
+                'parrots': [],
+                'chests': [],
+                'keys': [],
+                'rums': [],
+                'anchors': [],
+            }
+        self.whose_move = self.players_list[0]
+
+    def get_state(self):
+        return {
+            'cards': self.card_decks,
+            'whose_move': self.whose_move,
+            'is_started': self.is_started,
+        }
+
+    def transit_move_to_next_player(self):
+        if self.players_list.index(self.whose_move) == self.max_players - 1:
+            self.whose_move = self.players_list[0]
+        else:
+            self.whose_move = self.players_list[self.players_list.index(self.whose_move)+1]
+
+    def make_move(self, player_id, action):
+        if player_id != self.whose_move:
+            return False
+        if action == 'do_raid':
+            # игрок тянет карту из колоды в рейд
+            # перемешать колоду
+            random.shuffle(self.card_decks['treasure'])
+            # вытащить одну карту
+            pulled_card = self.card_decks['treasure'].pop()
+            # выцепить имя карты
+            pulled_card_name = pulled_card.split[0]
+            # пройтись по картам в рейде и сравнить
+            for card in self.card_decks['raid']:
+                raided_card_name = card.split[0]
+                if pulled_card_name == raided_card_name:
+                    # вытащена повторная карта, рейд уходит в сброс
+                    for i in range(len(self.card_decks['raid'])):
+                        self.card_decks['drop'].append(self.card_decks['raid'].pop())
+                    # смена хода
+                    self.transit_move_to_next_player()
+                    return 'raid_lost'
+            else:
+                return True
+        if action == 'end_raid':
+            for i in range(len(self.card_decks['raid'])):
+                # взять карту из рейда и получить ее имя
+                raided_card = self.card_decks['raid'].pop()
+                raided_card_name = raided_card.split()[0]
+                # найти нужную стопку у игрока и поместить туда каждую из карт
+                if raided_card_name == 'Штурвал':
+                    self.card_decks['player_id']['helms'].append(raided_card)
+                if raided_card_name == 'Нож':
+                    self.card_decks['player_id']['knives'].append(raided_card)
+                if raided_card_name == 'Пистолет':
+                    self.card_decks['player_id']['pistols'].append(raided_card)
+                if raided_card_name == 'Компас':
+                    self.card_decks['player_id']['compasses'].append(raided_card)
+                if raided_card_name == 'Свеча':
+                    self.card_decks['player_id']['candles'].append(raided_card)
+                if raided_card_name == 'Попугай':
+                    self.card_decks['player_id']['parrots'].append(raided_card)
+                if raided_card_name == 'Сундук':
+                    self.card_decks['player_id']['chests'].append(raided_card)
+                if raided_card_name == 'Ключ':
+                    self.card_decks['player_id']['keys'].append(raided_card)
+                if raided_card_name == 'Якорь':
+                    self.card_decks['player_id']['anchors'].append(raided_card)
+            # смена хода
+            self.transit_move_to_next_player()
+            return True
+
+
+
+app = FastAPI()
+
+# словарь с ключами - id игр и соответствующими значениями - объектами класса Game
+GAMES = {}
+
+# словарь с ключами - id игроков и соответствующими значениями - их именами
+PLAYERS = {}
+
+
+@app.get('/')
+def index():
+    return {
+        'status': 'ok',
+        'result': {
+            'message': 'Hello to the Treasure Island client!'
+        }
+    }
+
+
+@app.post('/register/')
+def register(player_name: str):
+    while True:
+        player_id = str(random.randint(10000, 99999))
+        if player_id not in PLAYERS:
+            break
+    PLAYERS[player_id] = player_name
+    return {
+        'status': 'ok',
+        'result': player_id
+    }
+
+
+@app.get('/get_games/')
+def get_games():
+    games_dicts_list = []
+    for game in GAMES:
+        if not GAMES[game].is_started:
+            games_dicts_list.append({
+                'id': GAMES[game].unique_id,
+                'players': '{}/{}'.format(len(GAMES[game].players_list), GAMES[game].max_players)
+            })
+    return {
+        'status': 'ok',
+        'result': games_dicts_list
+    }
+
+
+@app.post('/create_game/')
+def create_game(amount_of_players: int = 2):
+    if amount_of_players < 2 or amount_of_players > 4:
+        return {
+            'status': 'fail',
+            'error': 'Wrong players amount while creating game'
+        }
+    new_game = Game(amount_of_players)
+    GAMES[new_game.unique_id] = new_game
+    return {
+        'status': 'ok',
+        'result': new_game.unique_id
+    }
+
+
+@app.post('/enlist_for_game/')
+def enlist_for_a_game(game_id: str, player_id: str):
+    if GAMES[game_id].enlist_player(player_id):
+        return {
+            'status': 'ok',
+            'result': 'ok'
+        }
+    else:
+        return {
+            'status': 'fail',
+            'result': 'Enough players already'
+        }
+
+
+@app.get('/game_state')
+def get_game_state(game_id: str):
+    inspected_game = GAMES.get(game_id, False)
+    return {
+        'status': 'ok',
+        'result': inspected_game.get_state()
+    }
+
+
+@app.post('/make_move')
+def make_move(game_id, player_id, action):
+    pass
+
+
+if __name__ == '__main__':
+    uvicorn.run(
+        'server:app',
+        host='127.0.0.1',
+        port=5001,
+        reload=True
+    )
